59c9d3d85f178a4cc8446d3238e54894dc3b97da
Olesia <Swanriel@gmail.com>
2025-06-21 04:45:29 +0300

    Initial setup

---
+++ gendiff-project/gendiff.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander'
+
+const program = new Command()
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .option('-h, --help', 'display help for command')
+
+program.parse(process.argv)
+
+if (program.opts().help) {
+  program.help()
+}
