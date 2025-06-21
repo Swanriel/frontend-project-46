982413d40b36304a1a3b8a46a7eaf87f3283e909
Olesia <Swanriel@gmail.com>
2025-06-21 04:48:23 +0300

    Add file arguments and format option to CLI

--- gendiff-project/gendiff.js
+++ gendiff-project/gendiff.js
@@ -8,10 +8,13 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-h, --help', 'display help for command')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2) => {
+    console.log(`Comparing files:
+    File 1: ${filepath1}
+    File 2: ${filepath2}
+    Format: ${program.opts().format}`)
+  })
 
 program.parse(process.argv)
-
-if (program.opts().help) {
-  program.help()
-}
