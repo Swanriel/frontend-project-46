7f0b5c70026631eeb6026d78c18102fcd1c522b0
Olesia <Swanriel@gmail.com>
2025-06-22 04:26:50 +0300

    yaml

--- gendiff-project/src/parsers.js
+++ gendiff-project/src/parsers.js
@@ -1,10 +1,20 @@
-import fs from 'fs'
+import { readFileSync } from 'fs'
 import path from 'path'
+import { load } from 'js-yaml'
 
 const parseFile = (filepath) => {
-  const absolutePath = path.resolve(process.cwd(), filepath)
-  const data = fs.readFileSync(absolutePath, 'utf-8')
-  return JSON.parse(data)
-};
+  const fileExtension = path.extname(filepath).toLowerCase()
+  const fileContent = readFileSync(filepath, 'utf-8')
+
+  switch (fileExtension) {
+    case '.json':
+      return JSON.parse(fileContent)
+    case '.yml':
+    case '.yaml':
+      return load(fileContent)
+    default:
+      throw new Error(`Unsupported format: ${fileExtension}`)
+  }
+}
 
 export default parseFile
