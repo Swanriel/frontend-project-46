2af02712142b6012b5229d93558b159d42c34854
Olesia <Swanriel@gmail.com>
2025-06-22 05:40:19 +0300

    json

---
+++ gendiff-project/src/formatters/plain.js
@@ -0,0 +1,32 @@
+const formatValue = (value) => {
+  if (typeof value === 'object' && value !== null) return '[complex value]'
+  if (typeof value === 'string') return `'${value}'`
+  return String(value)
+}
+
+const buildPlainLines = (diff, path = '') => {
+  const lines = diff.flatMap((node) => {
+    const currentPath = path ? `${path}.${node.key}` : node.key
+
+    switch (node.type) {
+      case 'added':
+        return `Property '${currentPath}' was added with value: ${formatValue(node.value)}`
+      case 'removed':
+        return `Property '${currentPath}' was removed`
+      case 'updated':
+      case 'changed':
+        return `Property '${currentPath}' was updated. From ${formatValue(node.oldValue)} to ${formatValue(node.newValue)}`
+      case 'nested':
+        return buildPlainLines(node.children, currentPath)
+      case 'unchanged':
+        return []
+      default:
+        throw new Error(`Unknown node type: ${node.type}`)
+    }
+  })
+
+  return lines.join('\n')
+}
+
+export default (diff) => buildPlainLines(diff)
+
--- gendiff-project/src/index.js
+++ gendiff-project/src/index.js
@@ -1,18 +1,14 @@
-import parseFile from './parsers.js';
-import buildDiff from './diffBuilder.js';
-import formatStylish from './formatters/stylish.js';
+import parseFile from './parsers.js'
+import buildDiff from './diffBuilder.js'
+import getFormatter from './formatters/index.js'
 
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
-  const data1 = parseFile(filepath1);
-  const data2 = parseFile(filepath2);
-  const diff = buildDiff(data1, data2);
+  const data1 = parseFile(filepath1)
+  const data2 = parseFile(filepath2)
+  const diff = buildDiff(data1, data2)
   
-  switch (format) {
-    case 'stylish':
-      return formatStylish(diff);
-    default:
-      throw new Error(`Unknown format: ${format}`);
-  }
-};
+  const formatter = getFormatter(format);
+  return formatter(diff)
+}
 
-export default genDiff;
+export default genDiff
