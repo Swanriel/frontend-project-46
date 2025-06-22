bd168c4bca5306c95678a975d68ec5bde9a73c20
Olesia <Swanriel@gmail.com>
2025-06-22 05:51:37 +0300

    updated asciinema

--- gendiff-project/src/diffBuilder.js
+++ gendiff-project/src/diffBuilder.js
@@ -1,18 +1,21 @@
-import _ from 'lodash'
+import _ from 'lodash';
 
-const buildDiff = (data1, data2) => {
-  const keys = [...new Set([...Object.keys(data1), ...Object.keys(data2)])].sort()
+const buildDiff = (data1 = {}, data2 = {}) => {
+  const safeData1 = _.isObject(data1) ? data1 : {}
+  const safeData2 = _.isObject(data2) ? data2 : {}
+  
+  const keys = _.union(_.keys(safeData1), _.keys(safeData2)).sort()
   
   return keys.map((key) => {
-    if (!Object.hasOwn(data2, key)) {
-      return { key, type: 'removed', value: data1[key] }
+    if (!_.has(safeData2, key)) {
+      return { key, type: 'removed', value: safeData1[key] }
     }
-    if (!Object.hasOwn(data1, key)) {
-      return { key, type: 'added', value: data2[key] }
+    if (!_.has(safeData1, key)) {
+      return { key, type: 'added', value: safeData2[key] }
     }
 
-    const value1 = data1[key]
-    const value2 = data2[key]
+    const value1 = safeData1[key]
+    const value2 = safeData2[key]
 
     if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
       return { 
--- gendiff-project/src/parsers.js
+++ gendiff-project/src/parsers.js
@@ -3,17 +3,22 @@ import path from 'path'
 import { load } from 'js-yaml'
 
 const parseFile = (filepath) => {
-  const fileExtension = path.extname(filepath).toLowerCase()
-  const fileContent = readFileSync(filepath, 'utf-8')
+  try {
+    const fileExtension = path.extname(filepath).toLowerCase()
+    const fileContent = readFileSync(filepath, 'utf-8')
 
-  switch (fileExtension) {
-    case '.json':
-      return JSON.parse(fileContent)
-    case '.yml':
-    case '.yaml':
-      return load(fileContent)
-    default:
-      throw new Error(`Unsupported format: ${fileExtension}`)
+    switch (fileExtension) {
+      case '.json':
+        return JSON.parse(fileContent) || {}
+      case '.yml':
+      case '.yaml':
+        return load(fileContent) || {}
+      default:
+        throw new Error(`Unsupported format: ${fileExtension}`)
+    }
+  } catch (e) {
+    console.error(`Error parsing file ${filepath}:`, e.message)
+    return {}
   }
 }
 
